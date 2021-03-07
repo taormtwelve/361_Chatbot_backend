288c503f72c1beffe061e378dcc09cee9621e982
taormtwelve <taormtwelve@gmail.com>
2021-03-08 03:33:08 +0700

    commit

--- app.py
+++ app.py
@@ -382,4 +382,4 @@ def ans():
             return f"An Error Occured: {e}"
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
+    app.run(debug=True)
